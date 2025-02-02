10d381f19518c99dc7fe7f6e07721c96e6b1e73f
Henrik Tolbøl <henrik.tolboel@gmail.com>
2025-02-02 11:35:09 +0100

    begin with Batch DDD

--- src/main/java/net/example/batchgateway/application/service/QueryBatchUseCaseService.java
+++ src/main/java/net/example/batchgateway/application/service/QueryBatchUseCaseService.java
@@ -3,8 +3,11 @@ package net.example.batchgateway.application.service;
 import io.micrometer.observation.ObservationRegistry;
 import net.example.batchgateway.application.domain.model.Batch;
 import net.example.batchgateway.application.port.GeneralError;
+import net.example.batchgateway.application.port.input.CreateBatchCommand;
+import net.example.batchgateway.application.port.input.CreateBatchUseCasePort;
 import net.example.batchgateway.application.port.input.FindBatchQuery;
 import net.example.batchgateway.application.port.input.QueryBatchUseCasePort;
+import net.example.batchgateway.application.port.output.BatchRepositoryPort;
 import net.example.utils.dichotomy.Result;
 import org.springframework.stereotype.Service;
 
@@ -13,25 +16,19 @@ import java.util.Optional;
 @Service
 class QueryBatchUseCaseService implements QueryBatchUseCasePort {
 
-    private final BatchAdapter batchAdapter;
+    private final BatchRepositoryPort batchRepository;
     private final ObservationRegistry observationRegistry;
 
-    public QueryBatchUseCaseService(final BatchAdapter batchAdapter,
+
+    public QueryBatchUseCaseService(final BatchRepositoryPort batchRepository,
                                     final ObservationRegistry observationRegistry) {
-        this.batchAdapter = batchAdapter;
+        this.batchRepository = batchRepository;
         this.observationRegistry = observationRegistry;
     }
 
 
     @Override
     public Result<Optional<Batch>, GeneralError> findBatchById(FindBatchQuery command) {
-
-        // check allowance by tenantId and userId
-        // find Batch via BatchAdapter
-        // return Batch
-
-        Result<Optional<Batch>, GeneralError> result = batchAdapter.findById(command.batchId());
-
-        return result;
+        return batchRepository.findById(command.batchId());
     }
 }
